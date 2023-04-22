4fdb26f53b6a716de39f9d833f45c6fa3886f032
Charles Graham <crg0905@gmail.com>
2023-04-22 11:06:04 -0700

    Created app.js file

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const request = require("request-promise");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// So our app can parse JSON data
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send("My simple API!");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
