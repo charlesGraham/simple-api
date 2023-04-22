b8ea415b3761d16df6d08d634cfb19e696114fe0
Charles Graham <crg0905@gmail.com>
2023-04-22 11:54:26 -0700

    Adjusted to hide my personal API key

--- app.js
+++ app.js
@@ -4,11 +4,28 @@ const request = require("request-promise");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+const generateScraperUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
 // So our app can parse JSON data
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send("My simple API!");
+});
+
+// Get product details
+app.get('/products/:productId', async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`);
+
+    res.json(JSON.parse(response));
+  } catch (err) {
+    res.json(err);
+    console.log(err);
+  }
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
